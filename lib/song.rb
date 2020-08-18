4421941d3db32e083af257caca9485997ff47cb3
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-18 10:27:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,22 @@ attr_accessor :name :artist :genre
 @@count = 0
 @@artists = []
 @@genres = []
+
 def initialize
+  #no parameters because name artist genre are all optoinal
   @@count += 1
+end
+
+def count
+  @@count #can't use attr.. because these are not regular instnace variables
+end
+
+def artists
+  @@artists
+end
+
+def genres
+  @@genres
+end
 
 end
